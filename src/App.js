2753df0daa6a0624f647e1ce5a066ad4c4f7c008
MHood15 <mhood1598@gmail.com>
2020-07-09 21:23:54 -0600

    Added competition countdown timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown.js';
-import logo from './logo.svg';
-import github from './github.png';
 
 class App extends Component {
   render() {
+
+    const competitions = {
+      duration_length: 1,
+      duration_name: "Month(s)"
+    }
+
+    let plusDays = 0;
+    let finalMonths = 0;
+    let finalDays = 0;
+
     const currentDate = new Date();
-    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+    
+    const currentMonth = currentDate.getMonth() + 1
+    const currentDay = currentDate.getDate()
+    const daysUntilMonthEnd = 31 - currentDay //Close enough
+
+    if(competitions.duration_name === "Day(s)"){
+      plusDays = competitions.duration_length
+    }else if(competitions.duration_name === "Week(s)"){
+      plusDays = competitions.duration_length * 7
+    }else if(competitions.duration_name === "Month(s)"){
+      finalMonths = competitions.duration_length
+    }
+
+    if(plusDays <= daysUntilMonthEnd){
+      finalDays = plusDays;
+    }
+    while(plusDays > daysUntilMonthEnd){
+        finalMonths += 1;
+      if(plusDays >= 30){
+        plusDays -= 30
+      }else if(plusDays < 30){
+        plusDays -= daysUntilMonthEnd;
+      }
+      if(plusDays < 30){
+        finalDays = plusDays
+      }
+    }
+
+    function pad(n, width) {
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join(0) + n;
+    }
+
+    const year = currentDate.getFullYear()
+    const month = pad(currentMonth + finalMonths, 2)
+    const day = pad(currentDay + finalDays, 2)
+
+    
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Countdown</h2>
-
-          <a href="https://github.com/kristinbaumann/react-countdown" target="_blank">
-            <img src={github} alt="github" className="githubIcon" />
-            <span>View on Github</span>
-          </a>
-        </div>
-
-        <h3 className="title">Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):</h3>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+      <div>
+        <Countdown date={`${year}-${month}-${day}T00:00:00`} />
       </div>
     );
   }
